93fd1c70e46db55178d57ce3d8d120f0aec800ac
pawan8979 <p.negi8979@gmail.com>
2023-04-08 09:25:51 +0530

    event

---
+++ Event Handling/src/components/Book.jsx
@@ -0,0 +1,62 @@
+// const Book = (props) => {
+//   const { img, title, author, children } = props;
+//   return (
+//     <article className="book">
+//       <img src={img} alt={title} />
+//       <h2>{title}</h2>
+//       <h4>{author}</h4>
+//       {children}
+//     </article>
+//   );
+// };
+
+// const Book = (props) => {
+//   return (
+//     <article className="book">
+//       <img src={props.img} alt={props.title} />
+//       <h2>{props.title}</h2>
+//       <h4>{props.author}</h4>
+//       {props.children}
+//     </article>
+//   );
+// };
+
+// const Book = (props) => {
+//   const { img, title, author } = props;
+//   console.log(props);
+//   return (
+//     <article className="book">
+//       <img src={img} alt={title} />
+//       <h2>{title}</h2>
+//       <h4>{author}</h4>
+//     </article>
+//   );
+// };
+
+// const Book = ({ booky: { img, title, author } }) => {
+//   return (
+//     <article className="book">
+//       <img src={img} alt={title} />
+//       <h2>{title}</h2>
+//       <h4>{author}</h4>
+//     </article>
+//   );
+// };
+
+// using spread operator
+
+const Book = ({ img, title, author }) => {
+  const displayTitle = () => {
+    console.log(title);
+  };
+  return (
+    <article className="book">
+      <img src={img} alt={title} />
+      <h2>{title}</h2>
+      <button onClick={displayTitle}>Display Title</button>
+      <h4>{author}</h4>
+    </article>
+  );
+};
+
+export default Book;
